3e5c09ae22fb02b14790dba171a4e66f345dc53d
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2021-11-01 16:51:09 +0200

    changed App.xaml file under Prism, registered MainPage

--- MapNotepad/MapNotepad/App.xaml.cs
+++ MapNotepad/MapNotepad/App.xaml.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MapNotepad
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
         public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
+        }
+
+        #region --- Ovverides ---
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Services
+            //containerRegistry.RegisterInstance<IRepository>(Container.Resolve<Repository>());
+            
+
+            //Navigation
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            
+            NavigationService.NavigateAsync("/NavigationPage/MainPage");
+             
         }
 
         protected override void OnStart()
@@ -24,5 +46,7 @@ namespace MapNotepad
         protected override void OnResume()
         {
         }
+
+        #endregion
     }
 }
