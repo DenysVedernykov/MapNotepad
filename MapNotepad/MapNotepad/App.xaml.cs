a8619bcd15d8b19514c768a9f5f99e95cdd6efc0
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2021-11-02 19:49:48 +0200

    made layout StartView

--- MapNotepad/MapNotepad/App.xaml.cs
+++ MapNotepad/MapNotepad/App.xaml.cs
@@ -48,7 +48,7 @@ namespace MapNotepad
         {
             InitializeComponent();
             
-            NavigationService.NavigateAsync("/MainPageView");
+            NavigationService.NavigateAsync("/StartView");
              
         }
 
