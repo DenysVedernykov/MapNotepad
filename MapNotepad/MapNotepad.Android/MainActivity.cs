3e8cfff6a5472c3b83c8231b974fa8642b9231d5
Denys Vedernykov <razom.info@gmail.com>
2021-11-11 20:56:59 +0200

    added Xamarin.Forms.GoogleMaps package via NuGet; added a map to MainPage;

--- MapNotepad/MapNotepad.Android/MainActivity.cs
+++ MapNotepad/MapNotepad.Android/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 using Acr.UserDialogs;
+using Xamarin.Forms.GoogleMaps.Android;
 
 namespace MapNotepad.Droid
 {
@@ -19,6 +20,9 @@ namespace MapNotepad.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- MapNotepad/MapNotepad.iOS/AppDelegate.cs
+++ MapNotepad/MapNotepad.iOS/AppDelegate.cs
@@ -23,6 +23,9 @@ namespace MapNotepad.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+
+            Xamarin.FormsGoogleMaps.Init("AIzaSyAcBsMCF3pIsxYwe7ebpG7FiYXPpfozXFc");
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
