2d4d28be043d6badf245b5b32ebea504d856f728
Denys Vedernykov <razom.info@gmail.com>
2021-11-15 09:27:25 +0200

    made the layout SettingsPage

--- MapNotepad/MapNotepad/Views/AddPinsPage.xaml.cs
+++ MapNotepad/MapNotepad/Views/AddPinsPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms.Xaml;
 
 namespace MapNotepad.Views
@@ -14,6 +15,23 @@ namespace MapNotepad.Views
         public AddPinsPage()
         {
             InitializeComponent();
+        }
+
+        private void Map_MapClicked(object sender, Xamarin.Forms.GoogleMaps.MapClickedEventArgs e)
+        {
+            var pinTokyo = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Tokyo SKYTREE",
+                Address = "Sumida-ku, Tokyo, Japan",
+                Position = new Position(35.71d, 139.81d),
+                Rotation = 33.3f,
+                Tag = "id_tokyo",
+                IsVisible = true
+            };
+
+            var map = sender as Xamarin.Forms.GoogleMaps.Map;
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(pinTokyo.Position, Distance.FromMeters(5000)));
         }
     }
 }
